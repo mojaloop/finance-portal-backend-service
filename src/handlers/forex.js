dbaef07a41ab0d111819db3d0e821a56b02900ae
Vassilis Barzokas <vassilis.barzokas@modusbox.com>
2020-01-31 19:37:08 +0200

    Syntactic change to make it easier to read.

--- src/handlers/forex.js
+++ src/handlers/forex.js
@@ -5,25 +5,22 @@ const { getFxpRatesPerCurrencyChannel } = casaLib.admin.api;
 
 const handler = (router, routesContext) => {
     router.get('/forex/rates', async (ctx, next) => {
-        let fxpRatesPerCurrencyChannel;
-
         try {
-            fxpRatesPerCurrencyChannel = await getFxpRatesPerCurrencyChannel(
+            const fxpRatesPerCurrencyChannel = await getFxpRatesPerCurrencyChannel(
                 routesContext.config.fxpEndpoint,
             );
+
+            ctx.response.body = fxpRatesPerCurrencyChannel;
+            ctx.response.status = 200;
+
+            await next();
         } catch (error) {
             routesContext.log('Error', util.inspect(error, { depth: 10 }));
             ctx.response.body = { msg: 'FXP API Error' };
             ctx.response.status = 502;
 
             await next();
-
-            return;
         }
-
-        ctx.response.body = fxpRatesPerCurrencyChannel;
-        ctx.response.status = 200;
-        await next();
     });
 };
 
