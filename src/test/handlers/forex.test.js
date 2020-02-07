7e6257166367f80048af2f7ead263b50aa6ee815
Vassilis Barzokas <vbarzokas@gmail.com>
2020-02-07 13:52:09 +0200

    Added route `POST /fxp/rates/:currencyPair`

--- src/test/handlers/forex.test.js
+++ src/test/handlers/forex.test.js
@@ -43,3 +43,32 @@ describe('GET /forex/rates', () => {
         });
     });
 });
+
+describe('POST /forex/rates/:currencyPair', () => {
+    casaLib.admin.api.createFxpRateForCurrencyChannel = jest.fn();
+
+    afterEach(() => {
+        casaLib.admin.api.createFxpRateForCurrencyChannel.mockClear();
+    });
+
+    describe('Failures:', () => {
+        test('should return 502 in case `getFxpRatesPerCurrencyChannel` fails.', async () => {
+            casaLib.admin.api.createFxpRateForCurrencyChannel
+                .mockImplementation(jest.fn(() => { throw new Error('foo'); }));
+
+            const response = await request(server).get('/forex/rates/:currencyPair');
+            expect(response.status).toEqual(502);
+            expect(response.body).toEqual({ msg: 'FXP API Error' });
+        });
+    });
+    describe('Success:', () => {
+        test('should respond with a valid object propagated from `getFxpRatesPerCurrencyChannel`.', async () => {
+            casaLib.admin.api.createFxpRateForCurrencyChannel
+                .mockImplementation(jest.fn(() => Promise.resolve()));
+
+            const response = await request(server).get('/forex/rates/:currencyPair');
+            expect(response.status).toEqual(202);
+            expect(response.body).toEqual();
+        });
+    });
+});
