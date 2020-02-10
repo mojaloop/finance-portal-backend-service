ca906554ef38ae1bc02de850fb4fc851f9135036
Vassilis Barzokas <vassilis.barzokas@modusbox.com>
2020-02-10 15:20:40 +0200

    Added route `POST /forex/rates/:currencyPair` (#26)
    * Added route `POST /fxp/rates/:currencyPair`
    
    * Propagate request's body to `createFxpRateForCurrencyChannel`
    
    * Update to latest finance-portal-lib
    
    * Update dependencies
    
    * Fixed tests
    
    Co-authored-by: Kamuela Franco <kamuela.franco@gmail.com>
    Co-authored-by: Matt Kingston <mattkingston@gmail.com>

--- src/handlers/forex.js
+++ src/handlers/forex.js
@@ -1,7 +1,7 @@
 const casaLib = require('@mojaloop/finance-portal-lib');
 const util = require('util');
 
-const { getFxpRatesPerCurrencyChannel } = casaLib.admin.api;
+const { getFxpRatesPerCurrencyChannel, createFxpRateForCurrencyChannel } = casaLib.admin.api;
 
 const handler = (router, routesContext) => {
     router.get('/forex/rates', async (ctx, next) => {
@@ -12,6 +12,27 @@ const handler = (router, routesContext) => {
 
             ctx.response.body = fxpRatesPerCurrencyChannel;
             ctx.response.status = 200;
+
+            await next();
+        } catch (error) {
+            routesContext.log('Error', util.inspect(error, { depth: 10 }));
+            ctx.response.body = { msg: 'FXP API Error' };
+            ctx.response.status = 502;
+
+            await next();
+        }
+    });
+
+    router.post('/forex/rates/:currencyPair', async (ctx, next) => {
+        try {
+            await createFxpRateForCurrencyChannel(
+                routesContext.config.fxpEndpoint,
+                ctx.params.currencyPair.toLowerCase(),
+                ctx.request.body,
+            );
+
+            ctx.response.body = null;
+            ctx.response.status = 202;
 
             await next();
         } catch (error) {
