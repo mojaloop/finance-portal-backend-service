ccb9739afd86d059add7d5353fe56112702d35d3
Vassilis Barzokas <vassilis.barzokas@modusbox.com>
2020-02-14 17:56:44 +0200

    Leverage an external settlement API for `PUT /settlement-window-commit/:settlementWindowId`

--- src/test/handlers/settlement-window-commit.test.js
+++ src/test/handlers/settlement-window-commit.test.js
@@ -1,144 +1,68 @@
+const portalLib = require('@mojaloop/finance-portal-lib');
 const request = require('supertest');
+const handlerHelpers = require('../../lib/handlerHelpers');
 const mockData = require('./mock-data');
 const support = require('./_support.js');
 
-const { Database } = support;
-
-jest.genMockFromModule('node-fetch');
-jest.mock('node-fetch', () => jest.fn().mockImplementation(() => Promise.resolve({
-    status: 200, statusText: 'OK', ok: true,
-})));
-
 let server;
 let db;
-const logger = () => {};
 
 beforeEach(async () => {
-    Database.getPaymentMatrixForUpdate = jest.fn()
-        .mockImplementation((conn, settlementId) => {
-            if (settlementId === '1') {
-                return Promise.resolve('[["XOF","1",-25],["XOF","2",-15],["XOF","2",15],["XOF","1","25"]]');
-            }
-            if (settlementId === '2') {
-                return Promise.resolve('[["XOF","1",-25],["XOF","1",-15],["XOF","2","15"],["XOF","3","-15"]]');
-            }
-            if (settlementId === '3') {
-                return Promise.resolve('[["XOF","1",-25],["XOF","1",-15],["XOF","2","15"],["XOF","3","-15"]]');
-            }
-            if (settlementId === '4') {
-                return Promise.resolve('[["XOF","1",-25],["XOF","1",-15],["XOF","2","15"],["XOF","3","-15"]]');
-            }
-            return null;
-        });
-    Database.updatePaymentMatrixState = jest.fn();
-
     db = support.createDb();
-    server = support.createServer(db, logger, Database);
+    server = support.createServer(db);
 });
 
 afterEach(async () => {
     server.close();
 });
 
-jest.mock('@mojaloop/finance-portal-lib', () => ({
-    admin: {
-        api: {
-            getParticipantAccounts: jest.fn()
-                .mockImplementation(() => Promise.resolve(
-                    [
-                        ...mockData.positionAccounts,
-                        ...mockData.settleSettlementWindow.settlementAccounts,
-                    ],
-                )),
-            getParticipants: jest.fn()
-                .mockImplementation(() => Promise.resolve(
-                    [
-                        ...mockData.dfsps,
-                    ],
-                )),
-            participantFundsOutPrepareReserve: jest.fn()
-                .mockImplementation((endpoint, participantName) => {
-                    if (participantName === 'testfsp3' || participantName === 'testfsp4') {
-                        throw new Error('This fsp should fail');
-                    }
-                    return true;
-                }),
-            participantFundsInReserve: jest.fn()
-                .mockImplementation((endpoint, participantName) => {
-                    if (participantName === 'testfsp4') {
-                        throw new Error('This fsp should fail');
-                    }
-                    return true;
-                }),
-        },
-    },
-    settlement: {
-        api: jest.fn(() => ({ // this is required because its a constructor
-            putSettlement: jest.fn().mockImplementation(() => ({
-            })),
-        })),
-    },
-    requests: {
-        HTTPResponseError: jest.fn().mockImplementation(() => ({
-        })),
-    },
-}));
-
 describe('PUT /settlement-window-commit/:settlementWindowId', () => {
-    test('should do funds in/out and settle window and return the settlement window with SETTLED status', async () => {
-        const response = await request(server)
-            .put(`/settlement-window-commit/${mockData.settleSettlementWindow.request[0].settlementWindowId}`)
-            .send(mockData.settleSettlementWindow.request[0].body);
-        expect(response.status).toEqual(200);
-        const expectedWindow = mockData.settlementWindowList
-            .find(a => a.settlementWindowId.toString()
-                === mockData.settleSettlementWindow.request[0].settlementWindowId);
-        expect(response.body).toEqual(expectedWindow);
+    portalLib.admin.api.commitSettlementWindow = jest.fn();
+    handlerHelpers.getSettlementWindows = jest.fn();
+
+    afterEach(() => {
+        portalLib.admin.api.commitSettlementWindow.mockClear();
+        handlerHelpers.getSettlementWindows.mockClear();
     });
 
-    test('should settle window and return the settlement window with SETTLED status, when there are no funds in/out pending', async () => {
-        const response = await request(server)
-            .put(`/settlement-window-commit/${mockData.settleSettlementWindow.request[0].settlementWindowId}`)
-            .send(mockData.settleSettlementWindow.request[0].body);
-        expect(response.status).toEqual(200);
-        const expectedWindow = mockData.settlementWindowList
-            .find(a => a.settlementWindowId.toString()
-                === mockData.settleSettlementWindow.request[0].settlementWindowId);
-        expect(response.body).toEqual(expectedWindow);
-    });
+    describe('Failures:', () => {
+        test('should return status code 502 in case `commitSettlementWindow` fails', async () => {
+            portalLib.admin.api.commitSettlementWindow.mockImplementation(jest.fn(() => { throw new Error('foo'); }));
 
-    test('should return status code 500 if one of the funds out fails', async () => {
-        const response = await request(server)
-            .put(`/settlement-window-commit/${mockData.settleSettlementWindow.request[3].settlementWindowId}`)
-            .send(mockData.settleSettlementWindow.request[3].body);
-        const expectedBody = {};
-        expect(response.status).toEqual(500);
-        expect(response.body).toEqual(expectedBody);
-    });
+            const response = await request(server)
+                .put(`/settlement-window-commit/${mockData.settleSettlementWindow.request[3].settlementWindowId}`)
+                .send(mockData.settleSettlementWindow.request[3].body);
+            expect(response.status).toEqual(502);
+            expect(response.body).toEqual({ msg: 'TMF API Error' });
+        });
 
-    test('should return status code 500 if a row lock is encountered', async () => {
-        Database.getPaymentMatrixForUpdate = jest.fn()
-            .mockImplementation(() => { throw { code: 'ER_LOCK_WAIT_TIMEOUT' }; }); // eslint-disable-line no-throw-literal
-        server = support.createServer(db, logger, Database);
-        const { body } = mockData.settleSettlementWindow.request[3];
-        const response = await request(server)
-            .put(`/settlement-window-commit/${body.settlementWindowId}`)
-            .send(body);
-        expect(Database.getPaymentMatrixForUpdate.mock.calls[0][1]).toEqual(body.settlementId);
-        expect(Database.getPaymentMatrixForUpdate.mock.calls.length).toEqual(1);
-        expect(response.status).toEqual(500);
-        expect(response.body).toEqual({});
-    });
+        test('should return a successful status code in case `getSettlementWindows` fails', async () => {
+            portalLib.admin.api.commitSettlementWindow
+                .mockImplementation(jest.fn(() => Promise.resolve()));
+            handlerHelpers.getSettlementWindows.mockImplementation(jest.fn(() => { throw new Error('foo'); }));
 
-    test('should return status code 500 and only save failed transactions to the db if one fails', async () => {
-        const response = await request(server)
-            .put(`/settlement-window-commit/${mockData.settleSettlementWindow.request[3].settlementWindowId}`)
-            .send(mockData.settleSettlementWindow.request[3].body);
-        const expectedBody = {};
-        const expectedArgs = JSON.stringify([['XOF', '3', '-15']]);
-        expect(Database.updatePaymentMatrixState.mock.calls.length).toBe(1);
-        expect(Database.updatePaymentMatrixState.mock.calls[0][2]).toEqual(expectedArgs);
-        expect(response.status).toEqual(500);
-        expect(response.body).toEqual(expectedBody);
+            const response = await request(server)
+                .put(`/settlement-window-commit/${mockData.settleSettlementWindow.request[3].settlementWindowId}`)
+                .send(mockData.settleSettlementWindow.request[3].body);
+            expect(response.status).toEqual(200);
+            expect(response.body).toEqual({});
+        });
+    });
+    describe('Success:', () => {
+        test('should respond with a valid and latest settlement window in case it succeeds.', async () => {
+            portalLib.admin.api.commitSettlementWindow
+                .mockImplementation(jest.fn(() => Promise.resolve()));
+            handlerHelpers
+                .getSettlementWindows
+                .mockImplementation(
+                    jest.fn(() => Promise.resolve(mockData.settlementWindowList)),
+                );
+
+            const response = await request(server)
+                .put(`/settlement-window-commit/${mockData.settleSettlementWindow.request[3].settlementWindowId}`)
+                .send(mockData.settleSettlementWindow.request[3].body);
+            expect(response.status).toEqual(200);
+            expect(response.body).toEqual(mockData.settlementWindowList[0]);
+        });
     });
 });
