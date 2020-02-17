0b809a28450825089ce8520e946e36ad1bf5b958
James Bush <james.bush@modusbox.com>
2020-02-17 14:05:26 +0000

    make flags const and declarative

--- src/handlers/featureFlags.js
+++ src/handlers/featureFlags.js
@@ -1,11 +1,6 @@
 const handler = (router, routesContext) => {
     router.get('/featureflags', async (ctx, next) => {
-        let flags = {};
-
-        // dont barf if we dont have config or config.featureFlags set
-        if (routesContext.config && routesContext.config.featureFlags) {
-            flags = routesContext.config.featureFlags;
-        }
+        const flags = (routesContext.config && routesContext.config.featureFlags) ? routesContext.config.featureFlags : {};
 
         ctx.response.body = flags;
         ctx.response.status = 200;
