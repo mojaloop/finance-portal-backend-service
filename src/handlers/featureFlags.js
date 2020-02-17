ed71c1b0515e8f089b19d7b0bd77b3969a412136
James Bush <james.bush@modusbox.com>
2020-02-17 13:26:34 +0000

    fix linting errors

--- src/handlers/featureFlags.js
+++ src/handlers/featureFlags.js
@@ -2,8 +2,8 @@ const handler = (router, routesContext) => {
     router.get('/featureflags', async (ctx, next) => {
         let flags = {};
 
-        //dont barf if we dont have config or config.featureFlags set
-        if(routesContext.config && routesContext.config.featureFlags) {
+        // dont barf if we dont have config or config.featureFlags set
+        if (routesContext.config && routesContext.config.featureFlags) {
             flags = routesContext.config.featureFlags;
         }
 
