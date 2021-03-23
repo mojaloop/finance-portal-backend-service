d38ae22917d5d9b67f3811dd61a64ec3d84b31d3
msk- <mattkingston@gmail.com>
2021-03-23 17:08:58 +0000

    Pass through response status and error information from central settlement for settlement window close

--- src/handlers/settlement-window-close.js
+++ src/handlers/settlement-window-close.js
@@ -7,33 +7,34 @@ const handler = (router, routesContext) => {
     router.put('/settlement-window-close/:settlementWindowId', async (ctx, next) => {
         const { startDate: fromDateTime, endDate: toDateTime } = ctx.request.body;
 
-        try {
-            const resp = await axios.post(
-                `${routesContext.config.centralSettlementsEndpoint}/settlementWindows/${ctx.params.settlementWindowId}`,
-                {
-                    state: 'CLOSED',
-                    reason: 'Finance portal user request',
-                },
-            );
-            if (resp.status === 200) {
-                ctx.response.status = 200;
-            } else {
-                ctx.response.status = 502;
-            }
-        } catch (err) {
-            ctx.response.status = 502;
-        } finally {
-            // this delay is introduced in order to give the external settlement API enough time
+        const resp = await axios.post(
+            `${routesContext.config.centralSettlementsEndpoint}/settlementWindows/${ctx.params.settlementWindowId}`,
+            {
+                state: 'CLOSED',
+                reason: 'Finance portal user request',
+            },
+            {
+                validateStatus: null,
+            },
+        );
+
+        ctx.response.status = resp.status;
+
+        if (resp.status === 200) {
+            // this delay is introduced in order to give the central settlement API enough time
             // to close the window
-            // TODO revise the implementation of the external settlement API so it sends back an
-            //  acknowledgment somehow
+            // TODO revise the implementation of the central settlement API so it sends back an
+            // acknowledgment somehow
             await sleep(3000);
             ctx.response.body = await handlerHelpers.getSettlementWindows(
                 routesContext,
                 fromDateTime,
                 toDateTime,
             );
+        } else {
+            ctx.response.body = resp.data;
         }
+
         await next();
     });
 };
