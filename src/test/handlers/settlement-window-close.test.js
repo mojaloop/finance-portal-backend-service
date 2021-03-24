c0ac4760539a257defad1a81e1adcd7339878f7e
Matt Kingston <mattkingston@gmail.com>
2021-03-24 10:34:56 +0000

    Pass through response status and error information from central settlement for settlement window close (#63)

--- src/test/handlers/settlement-window-close.test.js
+++ src/test/handlers/settlement-window-close.test.js
@@ -28,25 +28,23 @@ describe('PUT /settlement-window-close/:settlementWindowId', () => {
         expect(response.body).toEqual(expectedWindowList);
     });
 
-    test('should return status code 502 if fails to close the window because the settlement endpoint responds non 202 status', async () => {
+    test('should return status code 502 and error information if fails to close the window because the settlement endpoint responds non 202 status', async () => {
+        const data = { errorInformation: 'error information' };
         axios.post = jest.fn().mockImplementationOnce(() => Promise.resolve({
-            status: 403, statusText: 'FORBIDDEN', ok: false,
+            status: 403, statusText: 'FORBIDDEN', ok: false, data,
         }));
         const response = await request(server)
             .put(`/settlement-window-close/${mockData.settleSettlementWindow.request[2].settlementWindowId}`)
             .send(mockData.settleSettlementWindow.request[2].body);
-        expect(response.status).toEqual(502);
-        const expectedWindowList = mockData.settlementWindowList;
-        expect(response.body).toEqual(expectedWindowList);
+        expect(response.status).toEqual(403);
+        expect(response.body).toEqual(data);
     });
 
-    test('should return status code 502 if fails to close the window', async () => {
+    test('should return status code 500 if fails to close the window', async () => {
         axios.post = jest.fn().mockImplementationOnce(() => Promise.reject(new Error('Error')));
         const response = await request(server)
             .put(`/settlement-window-close/${mockData.settleSettlementWindow.request[2].settlementWindowId}`)
             .send(mockData.settleSettlementWindow.request[2].body);
-        expect(response.status).toEqual(502);
-        const expectedWindowList = mockData.settlementWindowList;
-        expect(response.body).toEqual(expectedWindowList);
+        expect(response.status).toEqual(500);
     });
 });
