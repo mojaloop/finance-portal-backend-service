06caa0f6cbb616fba131066f5bcf98bddde1bb27
shashi165 <33355509+shashi165@users.noreply.github.com>
2021-08-26 08:13:59 -0500

    fix: report generation issue (#78)
    * fixed report generation issue
    
    * fixed audit issue
    
    * fixed audit issue
    
    * fixed audit issue

--- src/lib/reportsUtil.js
+++ src/lib/reportsUtil.js
@@ -2,77 +2,18 @@
 
 const { writeFile, unlink } = require('fs');
 const { parse } = require('qs');
-const { json2csv } = require('json-2-csv');
-
-const reportIds = {
-    312: [
-        'senderDFSPId',
-        'senderDFSPName',
-        'receiverDFSPId',
-        'receiverDFSPName',
-        'hubTxnID',
-        'transactionType',
-        'natureOfTxnType',
-        'requestDate',
-        'createdDate',
-        'modificationDate',
-        'settlementDate',
-        'senderCountryCurrencyCode',
-        'receiverCountryCurrencyCode',
-        'senderId',
-        'receiverId',
-        'reconciliationAmount',
-        'receiverNameStatus',
-        'pricingOption',
-        'receiverKYCLevelStatus',
-        'status',
-        'errorCode',
-        'senderDFSPTxnID',
-        'receiverDFSPTxnID',
-        'settlementWindowId'],
-
-    644: [
-        'settlementWindowId',
-        'fspId',
-        'windowOpen',
-        'windowClose',
-        'numTransactions',
-        'netPosition',
-        'state',
-    ],
-};
 
 /**
  * Generates a csv format of report and saves it.
  *
  * @param {Object} report    Reports from jasperserver.
  * @param {String} filename  Name of file to save.
- * @param {String} reportId  Id of requested report.
  * @returns {void}           Creates and saves file.
  */
-const generateReport = async (report, filename, reportId) => {
-    const options = {
-        delimiter: {
-            wrap: '"', // Double Quote (") character
-            field: ',', // Comma field delimiter
-            eol: '\n', // Newline delimiter
-        },
-        prependHeader: true,
-        sortHeader: false,
-        excelBOM: true,
-        trimHeaderValues: true,
-        trimFieldValues: true,
-        keys: reportIds[reportId],
-    };
-
-    json2csv(report, (err, csv) => {
-        if (err) throw err;
-
-        writeFile(filename, csv, (err2) => {
-            if (err2) throw err2;
-        });
-    },
-    options);
+const generateReport = async (report, filename) => {
+    writeFile(filename, report, (err2) => {
+        if (err2) throw err2;
+    });
 };
 
 /**
