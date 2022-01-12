965673b8ff6f1fa43f0739006dd246dccb75651f
shashi165 <33355509+shashi165@users.noreply.github.com>
2022-01-12 06:59:31 -0600

    feat: added external settlement endpoint (#85)
    * added external settlement endpoint
    
    @shashi165 thanks for the PR.
    
    I accept this PR, but note inconsistencies in Settlement Endpoint configuration and backwards-compatibility support must be addressed in future as part of this story --> https://github.com/mojaloop/project/issues/2639

--- src/handlers/settlement-window-close.js
+++ src/handlers/settlement-window-close.js
@@ -7,32 +7,53 @@ const handler = (router, routesContext) => {
     router.put('/settlement-window-close/:settlementWindowId', async (ctx, next) => {
         const { startDate: fromDateTime, endDate: toDateTime } = ctx.request.body;
 
-        const resp = await axios.post(
-            `${routesContext.config.centralSettlementsEndpoint}/settlementWindows/${ctx.params.settlementWindowId}`,
-            {
-                state: 'CLOSED',
-                reason: 'Finance portal user request',
-            },
-            {
-                validateStatus: null,
-            },
-        );
-
-        ctx.response.status = resp.status;
-
-        if (resp.status === 200) {
-            // this delay is introduced in order to give the central settlement API enough time
-            // to close the window
-            // TODO revise the implementation of the central settlement API so it sends back an
-            // acknowledgment somehow
-            await sleep(3000);
-            ctx.response.body = await handlerHelpers.getSettlementWindows(
-                routesContext,
-                fromDateTime,
-                toDateTime,
+        // if an external settlement service is provided use that
+        if (routesContext.config.externalSettlementsEndpoint) {
+            try {
+                const resp = await axios.post(`${routesContext.config.externalSettlementsEndpoint}`, {});
+                if (resp.status === 202) {
+                    ctx.response.status = 200;
+                } else {
+                    ctx.response.status = 500;
+                }
+            } catch (err) {
+                ctx.response.status = 500;
+            } finally {
+                // this delay is introduced in order to give the external settlement API enough time
+                // to close the window
+                // TODO revise the implementation of the external settlement API so it sends back an
+                //  acknowledgment somehow
+                await sleep(3000);
+                ctx.response.body = await handlerHelpers.getSettlementWindows(
+                    routesContext,
+                    fromDateTime,
+                    toDateTime,
+                );
+            }
+        } else { // use the default OSS settlement service
+            const resp = await axios.post(
+                `${routesContext.config.centralSettlementsEndpoint}/settlementWindows/${ctx.params.settlementWindowId}`,
+                {
+                    state: 'CLOSED',
+                    reason: 'Finance portal user request',
+                },
+                {
+                    validateStatus: null,
+                },
             );
-        } else {
-            ctx.response.body = resp.data;
+
+            ctx.response.status = resp.status;
+
+            if (resp.status === 200) {
+                await sleep(3000);
+                ctx.response.body = await handlerHelpers.getSettlementWindows(
+                    routesContext,
+                    fromDateTime,
+                    toDateTime,
+                );
+            } else {
+                ctx.response.body = resp.data;
+            }
         }
 
         await next();
--- src/test/handlers/settlement-window-close.test.js
+++ src/test/handlers/settlement-window-close.test.js
@@ -2,6 +2,7 @@ const request = require('supertest');
 const axios = require('axios');
 const mockData = require('./mock-data');
 const support = require('./_support');
+const config = require('../../config/config');
 
 jest.mock('node-fetch', () => () => require('./_support').mockAuthResponse);
 jest.mock('../../lib/permissions', () => ({ permit: jest.fn(() => true) }));
@@ -20,7 +21,21 @@ afterEach(async () => {
 });
 
 describe('PUT /settlement-window-close/:settlementWindowId', () => {
+    test('should close the settle window and return the list of all the settlement windows when external settlement endpoint is defined', async () => {
+        config.externalSettlementsEndpoint = 'http://external/settlement';
+        axios.post = jest.fn().mockImplementationOnce(() => Promise.resolve({
+            status: 202, statusText: 'OK', ok: true,
+        }));
+        const response = await request(server)
+            .put(`/settlement-window-close/${mockData.settleSettlementWindow.request[2].settlementWindowId}`)
+            .set(support.mockTokenHeader);
+        expect(response.status).toEqual(200);
+        const expectedWindowList = mockData.settlementWindowList;
+        expect(response.body).toEqual(expectedWindowList);
+    });
+
     test('should close the settle window and return the list of all the settlement windows', async () => {
+        config.externalSettlementsEndpoint = null;
         axios.post = jest.fn().mockImplementationOnce(() => Promise.resolve({
             status: 200, statusText: 'OK', ok: true,
         }));
@@ -33,6 +48,7 @@ describe('PUT /settlement-window-close/:settlementWindowId', () => {
     });
 
     test('should return status code 502 and error information if fails to close the window because the settlement endpoint responds non 202 status', async () => {
+        config.externalSettlementsEndpoint = null;
         const data = { errorInformation: 'error information' };
         axios.post = jest.fn().mockImplementationOnce(() => Promise.resolve({
             status: 403, statusText: 'FORBIDDEN', ok: false, data,
@@ -46,6 +62,17 @@ describe('PUT /settlement-window-close/:settlementWindowId', () => {
     });
 
     test('should return status code 500 if fails to close the window', async () => {
+        config.externalSettlementsEndpoint = null;
+        axios.post = jest.fn().mockImplementationOnce(() => Promise.reject(new Error('Error')));
+        const response = await request(server)
+            .put(`/settlement-window-close/${mockData.settleSettlementWindow.request[2].settlementWindowId}`)
+            .send(mockData.settleSettlementWindow.request[2].body)
+            .set(support.mockTokenHeader);
+        expect(response.status).toEqual(500);
+    });
+
+    test('should return status code 500 if fails to close the window using external settlement service', async () => {
+        config.externalSettlementsEndpoint = 'http://external/settlement';
         axios.post = jest.fn().mockImplementationOnce(() => Promise.reject(new Error('Error')));
         const response = await request(server)
             .put(`/settlement-window-close/${mockData.settleSettlementWindow.request[2].settlementWindowId}`)
